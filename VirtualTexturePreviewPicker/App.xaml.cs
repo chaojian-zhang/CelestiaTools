d4497709d52ad3fffb202467c663c44ac175d16f
Charles Zhang <szinubuntu@gmail.com>
2025-10-30 08:15:43 -0400

    Create project

---
+++ VirtualTexturePreviewPicker/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace VirtualTexturePreviewPicker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
